aad8e58b46420015083fb3486d3c82d34f891423
Litemage <jsimeone0105@gmail.com>
2022-02-17 19:30:57 -0600

    Added the rest of the admin view, and started basic events

--- Server/__init__.py
+++ Server/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for
 import os
+from flask_socketio import SocketIO
 
 def create_app():
     # CONFIGURE APP
@@ -10,12 +11,26 @@ def create_app():
         DATABASE = os.path.join(buzz.instance_path, "buzzer_system.sqlite")
     )
 
+    socketio = SocketIO(buzz)
+
     # ENSURE INSTANCE FOLDER EXISTS (where database is being kept)
     try:
         os.makedirs(buzz.instance_path)
     except OSError:
         pass
 
+    @socketio.on('client_connected')
+    def handle_connect(data):
+        print('[DEBUG] Client has connected')
+
+    @socketio.on('disconnect')
+    def handle_disconnect():
+        print('[DEBUG] Client has disconnected')
+
+    @socketio.on('queston_start')
+    def handle_question_start(data):
+        print('Name: {0}, Time: {1}'.format(data['qname'], data['qtime']))
+    
     @buzz.route('/')
     def hello_world():
         return render_template('hello_world.html')
