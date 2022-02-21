a3dc7d9c2ea53e195e42c6104e2c86137f6be244
Litemage <jsimeone0105@gmail.com>
2022-02-21 17:40:49 -0600

    V1 Database and User functionality

--- Buzzer/__init__.py
+++ Buzzer/__init__.py
@@ -1,16 +1,25 @@
-from flask import Flask, render_template, url_for, redirect, request
+from flask import Flask, render_template, url_for, redirect, request, flash, session, g
 import functools
 import os
 from flask_socketio import SocketIO
+from Buzzer.db import get_db, init_app
 
+
+# ================================== HELPERS ==================================
 
 def login_required(view):
     @functools.wraps(view)
     def wrapped_view(**kwargs):
         if g.user is None:
-            return redirect("login.html")
-    
+            print('[DEBUG] user is none')
+            return redirect("login")
+        return view(**kwargs)
+
+    print('[DEBUG] returning wrapped_view')
+    print(wrapped_view)
     return wrapped_view
+
+# ================================== CONFIG & INIT ==================================
 
 def create_app():
     # CONFIGURE APP
@@ -21,13 +30,29 @@ def create_app():
         DATABASE = os.path.join(buzz.instance_path, "buzzer_system.sqlite")
     )
 
-    socketio = SocketIO(buzz)
+    #init database
+    init_app(buzz)
 
     # ENSURE INSTANCE FOLDER EXISTS (where database is being kept)
     try:
         os.makedirs(buzz.instance_path)
     except OSError:
         pass
+
+    @buzz.before_request
+    def load_logged_in_user():
+        user_id = session.get('user_id')
+
+        if user_id is None:
+            g.user = None
+        else:
+            g.user = get_db().execute(
+                'SELECT * FROM users WHERE id = ?', (user_id,)
+            ).fetchone()
+
+    # ================================== SOCKETS ==================================
+
+    socketio = SocketIO(buzz)
 
     @socketio.on('connect')
     def handle_connect(data):
@@ -43,13 +68,17 @@ def create_app():
         print('[DEBUG] Received question stop from admin')
         socketio.emit('server_question_stop')
     
+    # ================================== ROUTES ==================================
+
     @buzz.route('/')
     def hello_world():
         return render_template('hello_world.html')
     
 
     @buzz.route('/buzzer')
+    @login_required
     def buzzer():
+        print('Going to render template')
         return render_template('buzzer.html')
 
     @buzz.route('/admin')
@@ -57,12 +86,40 @@ def create_app():
         return render_template('admin.html')
 
     @buzz.route('/login', methods=('GET', 'POST'))
-    def login():
-        if request.method == 'GET':
-            return render_template('login.html')
-        elif request.method == 'POST':
-            # Take in username and put it in database
-            raise NotImplementedError
+    def login():     
+        if request.method == 'POST':
+            username = request.form['username']
+            db = get_db()
+            error = None
+
+            if not username:
+                error = 'No username received, can\'t write'
+            
+            if error is None:
+                try:
+                    db.execute(
+                        "INSERT INTO users (username) VALUES (?)",
+                        (username,)
+                    )
+                    db.commit()
+                except db.IntegrityError:
+                    error = f"User {username} already registered"
+                    print("Username Already exists")
+                else:
+                    user = db.execute(
+                        'SELECT * FROM users WHERE username = ?',
+                        (username,)
+                    ).fetchone()
+                    session.clear()
+                    session['user_id'] = user['id']
+                    print("[DEBUG] tried to redirect")
+                    print(redirect('buzzer'))
+                    return redirect('buzzer')
+            print(f'[DEBUG] error raised: {error}')
+            flash(error)
+            
+        return render_template('login.html')
+            
 
 
     return buzz
